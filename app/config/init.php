5a6cbcc9538ff4a576423a13cbf63188e0b59cbc
root <nikos>
2017-05-10 16:09:46 -0400

    Php errors are hidden in prod

--- app/config/init.php
+++ app/config/init.php
@@ -31,5 +31,11 @@ if (!defined('ENV')) {
     }
 
     define('ENV', $env);
+    if (ENV == 'prod') {
+        ini_set("display_errors", 0);
+        ini_set("log_errors", 1);
 
+        //Define where do you want the log to go, syslog or a file of your liking with
+        //ini_set("error_log", "syslog"); // or ini_set("error_log", "/path/to/syslog/file");
+    }
 }
