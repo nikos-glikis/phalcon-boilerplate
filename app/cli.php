f101530a8ee09549a240fc6c769b88acc8068486
root <nikos>
2017-05-13 17:31:27 -0400

    Autoloader in cli.

--- app/cli.php
+++ app/cli.php
@@ -8,6 +8,8 @@ use Phalcon\Loader;
 // Using the CLI factory default services container
 $di = new CliDI();
 
+define('BASE_PATH', dirname(__DIR__));
+define('APP_PATH', BASE_PATH . '/app');
 
 /**
  * Register the autoloader and tell it to register the tasks directory
@@ -22,6 +24,14 @@ $loader->registerDirs(
 
 $loader->register();
 
+/**
+ * Handle routes
+ */
+include APP_PATH . '/config/router.php';
+/**
+ * Read services
+ */
+include APP_PATH . '/config/services.php';
 
 // Load the configuration file (if any)
 
@@ -33,6 +43,11 @@ if (is_readable($configFile)) {
     $di->set("config", $config);
 }
 
+
+/**
+ * Include Autoloader
+ */
+include APP_PATH . '/config/loader.php';
 
 // Create a console application
 $console = new ConsoleApp();
