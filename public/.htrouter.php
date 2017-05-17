3a84a683d5a2c06943e18a1bc158be64cfcbe640
root <nikos>
2017-05-17 14:36:13 -0400

    Updated router.

--- public/.htrouter.php
+++ public/.htrouter.php
@@ -20,14 +20,17 @@
 */
 
 if (!file_exists(__DIR__ . '/' . $_SERVER['REQUEST_URI'])) {
-    list($url, $other) = explode('?', $_SERVER['REQUEST_URI']);
-    $_GET['_url'] = $url;
-    if ($other) {
-        $params = explode('&', $other);
-        foreach ($params as $param) {
-            list($key, $value) = explode('=', $param);
-            $_GET[$key] = $value;
+    if (strpos($_SERVER['REQUEST_URI'], '?') !== false) {
+        list($url, $other) = explode('?', $_SERVER['REQUEST_URI']);
+        $_GET['_url'] = $url;
+        if ($other && strpos($other, '&') !== false) {
+            $params = explode('&', $other);
+            foreach ($params as $param) {
+                list($key, $value) = explode('=', $param);
+                $_GET[$key] = $value;
+            }
         }
     }
+
 }
 return false;
